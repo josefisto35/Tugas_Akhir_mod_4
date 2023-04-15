db192f0443fa33883d78baf9842a7233624398ac
josefisto35 <103529204+josefisto35@users.noreply.github.com>
2023-04-15 19:25:24 +0700

    Import module scikit-learn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from sklearn.linear_model import LinearRegression
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 from sklearn.model_selection import train_test_split
